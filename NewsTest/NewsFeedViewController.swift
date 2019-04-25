257d7084d9b7668640f401772beffa1c8a8198c5
Felföldy Tibor <tiboofficial@gmail.com>
2019-04-25 23:35:16 +0200

    Added navigation controller

--- NewsTest/NewsFeedViewController.swift
+++ NewsTest/NewsFeedViewController.swift
@@ -8,14 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class NewsFeedViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
     }
 
-
 }
+
 
