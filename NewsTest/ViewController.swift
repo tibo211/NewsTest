709e00a2d917ac7dcb0fa4f079586ea8ca26c6e9
Felföldy Tibor <tiboofficial@gmail.com>
2019-04-25 21:31:14 +0200

    Created project

---
+++ NewsTest/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  NewsTest
+//
+//  Created by Felföldy Tibor on 2019. 04. 25..
+//  Copyright © 2019. Felföldy Tibor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("test")
+    }
+
+
+}
+
